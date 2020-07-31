ff6c6a6ebc56f93fd3c648bfd0407188c3ac6ef5
Yangkro <907661446@qq.com>
2020-07-31 15:36:07 +0800

    Updates

---
+++ precache-manifest.1d49dcc4c99e5855d1c246d8ef9b6c51.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "223650a586a9e630920a1cf313801b54",
+    "url": "/Chat-ui/index.html"
+  },
+  {
+    "revision": "e1776958bb856714448e",
+    "url": "/Chat-ui/static/css/main.b5dd50bb.chunk.css"
+  },
+  {
+    "revision": "9945a5fe3f04c16ccf8b",
+    "url": "/Chat-ui/static/js/2.36ae2448.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Chat-ui/static/js/2.36ae2448.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1776958bb856714448e",
+    "url": "/Chat-ui/static/js/main.bda560f8.chunk.js"
+  },
+  {
+    "revision": "d3871a22283d4c4f60fe",
+    "url": "/Chat-ui/static/js/runtime-main.0f26946a.js"
+  },
+  {
+    "revision": "38fbcf2508acf8e8c1f34161986eb18f",
+    "url": "/Chat-ui/static/media/arrowMenuLeft.38fbcf25.svg"
+  },
+  {
+    "revision": "902284657f93e59d66f4fedc626bc7e2",
+    "url": "/Chat-ui/static/media/arrowMenuRight.90228465.svg"
+  },
+  {
+    "revision": "81e1ebc572237b7646cbea509ca01029",
+    "url": "/Chat-ui/static/media/arrowRight.81e1ebc5.svg"
+  },
+  {
+    "revision": "17dd804b531d69fc02755395938b78af",
+    "url": "/Chat-ui/static/media/bubbleTip.17dd804b.svg"
+  },
+  {
+    "revision": "8c44964bc137be0f0fa4e5479cf5d759",
+    "url": "/Chat-ui/static/media/call.8c44964b.svg"
+  },
+  {
+    "revision": "431967a9d080c377113242ad9ca9aea0",
+    "url": "/Chat-ui/static/media/camera.431967a9.svg"
+  },
+  {
+    "revision": "262e2ac1a583e7f82b50c685908c9317",
+    "url": "/Chat-ui/static/media/caretDown2.262e2ac1.svg"
+  },
+  {
+    "revision": "f01c5d90ead4abf08e3535fe60cd19de",
+    "url": "/Chat-ui/static/media/caret_down.f01c5d90.svg"
+  },
+  {
+    "revision": "cb7ebfa000435428f27c91b0480d4272",
+    "url": "/Chat-ui/static/media/clip.cb7ebfa0.svg"
+  },
+  {
+    "revision": "0d7a499ace94fba16dd8ca9762a7ee37",
+    "url": "/Chat-ui/static/media/closeCircle.0d7a499a.svg"
+  },
+  {
+    "revision": "07b8e41d9192f73e72cec6b9381e2158",
+    "url": "/Chat-ui/static/media/cross.07b8e41d.svg"
+  },
+  {
+    "revision": "16d09359316e0568b10601fbc4f59476",
+    "url": "/Chat-ui/static/media/face-female-1.16d09359.jpg"
+  },
+  {
+    "revision": "ea1ac5791a6998120dc092a03bdce490",
+    "url": "/Chat-ui/static/media/face-female-2.ea1ac579.jpg"
+  },
+  {
+    "revision": "d5e45589d0628923febc6e7d5a4e632e",
+    "url": "/Chat-ui/static/media/fileExcel.d5e45589.svg"
+  },
+  {
+    "revision": "6d14852f17f691f3cb1d9d3d4887c5e8",
+    "url": "/Chat-ui/static/media/fileImage.6d14852f.svg"
+  },
+  {
+    "revision": "7f5d7f5c44dd5aa68369a6fefc2fc82b",
+    "url": "/Chat-ui/static/media/filePdf.7f5d7f5c.svg"
+  },
+  {
+    "revision": "2e033eda1f6345b7f9254e97aeb8d226",
+    "url": "/Chat-ui/static/media/filePpt.2e033eda.svg"
+  },
+  {
+    "revision": "1924059abd3323dd3dc1fe36c585664b",
+    "url": "/Chat-ui/static/media/fileWord.1924059a.svg"
+  },
+  {
+    "revision": "ee803bafcd79d871ab97a3006619b8e6",
+    "url": "/Chat-ui/static/media/fileZip.ee803baf.svg"
+  },
+  {
+    "revision": "df35212e303d370d979df94a39f3293a",
+    "url": "/Chat-ui/static/media/microphone.df35212e.svg"
+  },
+  {
+    "revision": "3ea3ee01ac60c8ea6695b7074f02932c",
+    "url": "/Chat-ui/static/media/note-1.3ea3ee01.jpg"
+  },
+  {
+    "revision": "d6c000514fa113304e9cd56054553352",
+    "url": "/Chat-ui/static/media/note-2.d6c00051.jpg"
+  },
+  {
+    "revision": "73489071e5af4763ddfa4b33c52f79d4",
+    "url": "/Chat-ui/static/media/note-3.73489071.jpg"
+  },
+  {
+    "revision": "07a560890557d905f3350f3207e33ddb",
+    "url": "/Chat-ui/static/media/note-4.07a56089.jpg"
+  },
+  {
+    "revision": "144fb68fe8144a0bff8417a7996ba226",
+    "url": "/Chat-ui/static/media/note-5.144fb68f.jpg"
+  },
+  {
+    "revision": "8db6776b64720c598e75df9b99a87ea1",
+    "url": "/Chat-ui/static/media/note-6.8db6776b.jpg"
+  },
+  {
+    "revision": "b7baa4dbdfe24b654e819eb1dfe8dc82",
+    "url": "/Chat-ui/static/media/note-7.b7baa4db.jpg"
+  },
+  {
+    "revision": "b22d090ea3694a66629cc0ac3235e91f",
+    "url": "/Chat-ui/static/media/options.b22d090e.svg"
+  },
+  {
+    "revision": "ef66beb43a59cdf883379c4dfe1190a4",
+    "url": "/Chat-ui/static/media/photo1.ef66beb4.jpg"
+  },
+  {
+    "revision": "741674cc06249b95c2a8a0f4561e6fd8",
+    "url": "/Chat-ui/static/media/photo2.741674cc.jpg"
+  },
+  {
+    "revision": "2fb38cab4d1274a9188bc89eea40cef8",
+    "url": "/Chat-ui/static/media/photo3.2fb38cab.jpg"
+  },
+  {
+    "revision": "114eb45eefa4f27720ad54a6e833948b",
+    "url": "/Chat-ui/static/media/plane.114eb45e.svg"
+  },
+  {
+    "revision": "488dc79e07b5bfdcc0c1d328564a5577",
+    "url": "/Chat-ui/static/media/play.488dc79e.svg"
+  },
+  {
+    "revision": "de8f0052ce281c9e974f447197e85e9d",
+    "url": "/Chat-ui/static/media/plus.de8f0052.svg"
+  },
+  {
+    "revision": "67545644b35792c5007ea511eb963796",
+    "url": "/Chat-ui/static/media/replied.67545644.svg"
+  },
+  {
+    "revision": "9c0c696fb880e36f64b154a98deec4cc",
+    "url": "/Chat-ui/static/media/search.9c0c696f.svg"
+  },
+  {
+    "revision": "9e7b1025b0eecbace8d6375358223bf7",
+    "url": "/Chat-ui/static/media/smile.9e7b1025.svg"
+  },
+  {
+    "revision": "79dbf418e9a64b1a9b967223aeeefe19",
+    "url": "/Chat-ui/static/media/video-caller.79dbf418.jpg"
+  },
+  {
+    "revision": "8a1ba1924b39fc86a3f72e5dd0e19542",
+    "url": "/Chat-ui/static/media/wave.8a1ba192.svg"
+  }
+]);
